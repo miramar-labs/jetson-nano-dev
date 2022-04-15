d5342fd94a7f807881ee898bc06056e665e6d2e3
Aaron Cody <aaron@aaroncody.com>
2022-04-14 19:52:18 -0700

    update repos

--- utils/repos.sh
+++ utils/repos.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+git config --global url."git@github.com:".insteadOf "https://github.com/"
+git config --global user.email "aaron@aaroncody.com"
+git config --global user.name "Aaron Cody"
+git config --global push.default simple
 pushd ~
 git clone https://github.com/miramar-labs/interview-practice.git
 git clone https://github.com/fastai/fastai.git
