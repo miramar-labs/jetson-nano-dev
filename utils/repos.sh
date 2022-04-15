c7d942f086b415dd451898f96d636a302165da6e
Aaron Cody <aaron@aaroncody.com>
2022-04-14 23:55:46 -0700

    tweak to repos

--- utils/repos.sh
+++ utils/repos.sh
@@ -5,7 +5,6 @@ git config --global user.email "aaron@aaroncody.com"
 git config --global user.name "Aaron Cody"
 git config --global push.default simple
 pushd ~
-git clone https://github.com/miramar-labs/interview-practice.git
 git clone https://github.com/fastai/fastai.git
 git clone https://github.com/fastai/fastbook.git
 popd
