9c9491f2c4a13077c82bb1119c24cde81b78e2d9
Aaron Cody <aaron@aaroncody.com>
2022-04-14 19:24:30 -0700

    repos

---
+++ utils/repos.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+pushd ~
+git clone https://github.com/miramar-labs/interview-practice.git
+git clone https://github.com/fastai/fastai.git
+git clone https://github.com/fastai/fastbook.git
+popd
